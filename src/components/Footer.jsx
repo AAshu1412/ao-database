a910483b367c2bfef20f4e04df063d6555b493aa
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-06-09 08:48:19 +0530

    minor change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <div className="bg-orange-600 h-20 text-white flex justify-center items-center ">
+    <div className="bg-orange-600 h-20 text-white flex justify-center items-center mt-72">
       ashutoshsanjeevmittal@gmail.com 
     </div>
   );
